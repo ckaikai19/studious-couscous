916bd591ecf7df22d5794deff24b1d3f4059c012
chirstiankaikai <ckaikai19@gmail.com>
2022-07-25 21:55:38 -0400

    fixed bugs

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,7 +13,7 @@ const initdb = async () =>
   });
 
 // TODO Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => {
+export const putDb = async (id, value) => {
   console.log('PUT request recived');
   const jateDb = await openDB('jate', 1); 
   const tx = jateDb.transaction('jate', 'readwrite');
@@ -24,7 +24,7 @@ export const putDb = async (content) => {
 };
 
 // TODO Add logic for a method that gets all the content from the database
-export const getDb = async () => {
+export const getDb = async (value) => {
   console.log('Getting data from DB');
   const jateDb = await openDB('jate', 1); 
   const tx = jateDb.transaction('jate', 'readwrite');
